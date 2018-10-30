e614209149a71e1e4e3460abd9195413dc395754
xuchuanliangbt <xuchuanliangbt@hikbt.com>
2018-10-30 13:03:43 +0800

    增加spring boot子模块，查看源码使用

---
+++ spring-boot-source/src/main/java/com/snail/springbootsource/SpringBootSourceApplication.java
@@ -0,0 +1,12 @@
+package com.snail.springbootsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSourceApplication.class, args);
+    }
+}
