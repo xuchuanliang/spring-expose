39b4bea042fac2e0a1530f0ec3975e4cc644035e
xuchuanliangbt <xuchuanliangbt@hikbt.com>
2018-12-24 08:31:02 +0800

    提交

---
+++ spring-boot-source/src/main/java/com/snail/springbootsource/capter16/Test.java
@@ -0,0 +1,13 @@
+package com.snail.springbootsource.capter16;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+public class Test {
+
+    public static void test1(){
+        HttpClient httpClient = new HttpClient();
+        GetMethod getMethod = new GetMethod("www.baidu.com");
+
+    }
+}
